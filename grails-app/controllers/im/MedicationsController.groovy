4de95fc639d24d5584a28f345d6df3bda7e0dd5c
bunndan <brandon.ma@live.ca>
2014-06-19 13:40:44 -0400

    Implemented the new Medications page using Grails' protocols.

--- grails-app/controllers/im/MedicationsController.groovy
+++ grails-app/controllers/im/MedicationsController.groovy
@@ -1,11 +1,11 @@
 package im
 
+import org.springframework.dao.DataIntegrityViolationException
 import groovy.sql.Sql
 
-import java.util.Date
-import java.text.SimpleDateFormat
-
 class MedicationsController {
+    
+    static allowedMethods = [save: "POST", update: "POST", delete: ["GET", "POST"]]
     
     def dataSource // the Spring-Bean "dataSource" is auto-injected
     
@@ -13,7 +13,8 @@ class MedicationsController {
     def beforeInterceptor = [action:this.&auth]
     
     def index() {
-        redirect(action: "medications")
+        println("index  {params = " + params + "}");
+        redirect(action: "medications", params: params)
     }
     
     def auth() {
@@ -23,13 +24,13 @@ class MedicationsController {
         }
     }
     
-    
-    
     /** 
      * Main landing page for Medications tab.
      */
     def medications = {
+        println("medications");
         
+        /* OLD SQL CODE
         def db = new Sql(dataSource) // Create a new instance of groovy.sql.Sql with the DB of the Grails app
         
         def result = db.rows("SELECT um.name, um.dose, um.frequency, um.form, um.strength, um.start_date, " + 
@@ -37,13 +38,107 @@ class MedicationsController {
                              "WHERE um.user_id = l.user_id AND l.user_name = ? ", session.user.userName) // Perform the query
         
         [result: result] // return the results as model
+        */
+        
+        def db = new Sql(dataSource) // Create a new instance of groovy.sql.Sql with the DB of the Grails app
+        
+        def UserMedicationsInstanceList = db.rows(
+            "SELECT um.medication_id, um.name, um.dose, um.frequency, um.form, um.strength, um.start_date, " + 
+            " um.stop_date, um.refill_date, um.comments " + 
+            " FROM user_medications um, login l " + 
+            " WHERE um.user_id = l.user_id AND l.user_name = ? ", session.user.userName)
+        
+        [UserMedicationsInstanceList: UserMedicationsInstanceList]
     }
     
     
-    def add_medicine = {
-        println("working")
-        return false
+    def create() {
+        println("create");
+        [UserMedicationsInstance: new UserMedications(params)]
     }
     
+    def save() {
+        println("save");
+        
+        def UserMedicationsInstance = new UserMedications(params)
+        
+        if (!UserMedicationsInstance.save(flush: true)) {
+            println("didn't save :  " + UserMedicationsInstance.errors);
+            render(view: "create", model: [UserMedicationsInstance: UserMedicationsInstance])
+            return
+        }
+        redirect(action: "medications", params: params)
+    }
+    
+    def edit(Long id) {
+        println("edit : " + id);
+        
+        def UserMedicationsInstance = UserMedications.get(id)
+        
+        println("userMedications = " + UserMedicationsInstance + "   |   " + UserMedications)
+        if (!UserMedicationsInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'UserMedications.label', default: 'UserMedications'), id])
+            redirect(action: "medications")
+            return
+        }
+        
+        [userMedicationsInstance: UserMedicationsInstance]
+    }
+    
+    def update(Long id, Long version) {
+        println("update");
+        def UserMedicationsInstance = UserMedications.get(id)
+        if (!UserMedicationsInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'UserMedications.label', default: 'UserMedications'), id])
+            redirect(action: "medications")
+            return
+        }
+
+        if (version != null) {
+            if (UserMedicationsInstance.version > version) {
+                UserMedicationsInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                          [message(code: 'UserMedications.label', default: 'UserMedications')] as Object[],
+                          "Another user has updated this UserMedications while you were editing")
+                render(view: "edit", model: [UserMedicationsInstance: UserMedicationsInstance])
+                return
+            }
+        }
+
+        UserMedicationsInstance.properties = params
+
+        if (!UserMedicationsInstance.save(flush: true)) {
+            render(view: "edit", model: [UserMedicationsInstance: UserMedicationsInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.updated.message', args: [message(code: 'UserMedications.label', default: 'UserMedications'), UserMedicationsInstance.id])
+        redirect(action: "medications", id: UserMedicationsInstance.id)
+    }
+
+    def delete(Long id) {
+        println("delete")
+        def UserMedicationsInstance = UserMedications.get(id)
+        if (!UserMedicationsInstance) {
+            println("something failed")
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'UserMedications.label', default: 'UserMedications'), id])
+            redirect(action: "medications")
+            return
+        }
+
+        try {
+            UserMedicationsInstance.delete(flush: true)
+            flash.message = message(code: 'default.deleted.message', args: [message(code: 'UserMedications.label', default: 'UserMedications'), id])
+            redirect(action: "medications")
+        }
+        catch (DataIntegrityViolationException e) {
+            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'UserMedications.label', default: 'UserMedications'), id])
+            redirect(action: "medications", id: id)
+        }
+        
+    }
     
 }
+
+
+
+
--- grails-app/views/medications/medications.gsp
+++ grails-app/views/medications/medications.gsp
@@ -12,19 +12,18 @@
         
         <div id="main-body" style="clear: both">
             
-            <g:form action="add" class="form-button" >
-                <fieldset class="buttons">
-                    <g:submitButton name="add" class="add-button btn btn-primary" value="Add" />
-                </fieldset>
-            </g:form>
+            
+            <g:link class="add-button btn btn-primary form-button" action="create">
+                Add
+            </g:link>
             
             
             <h1>Current Medications</h1>
             <table class="table table-hover table-striped">
-				<caption>Medication that is currently being taken. Will automatically update when date expires.</caption>
-				<thead>
-					<tr>
-						<th class="table-name">Name</th>
+                <caption>Medication that is currently being taken. Will automatically update when date expires.</caption>
+                <thead>
+                    <tr>
+                        <th class="table-name">Name</th>
                         <th>Dose</th>
                         <th>Frequency</th>
                         <th>Form</th>
@@ -34,29 +33,33 @@
                         <th class="table-date">Refill Date</th>
                         <th class="table-reason">Reason for taking</th>
                         <th class="table-editdelete">Edit/Delete</th>
-					</tr>
-				</thead>
-				<tbody>
-				    <g:each in="${result}" var="data">
-					<tr>
-						<td>${data.name }</td>
-						<td>${data.dose }</td>
-						<td>${data.frequency }</td>
-						<td>${data.form }</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <g:each in="${UserMedicationsInstanceList }" var="data">
+                    <tr>
+                        <td>${data.name }</td>
+                        <td>${data.dose }</td>
+                        <td>${data.frequency }</td>
+                        <td>${data.form }</td>
                         <td>${data.strength }</td>
                         <td>${data.start_date }</td>
                         <td>${data.stop_date }</td>
                         <td>${data.refill_date }</td>
                         <td>${data.comments }</td>
                         <td class="table-editdelete">
-                            <i class="icon-cog"></i>
-                            <i class="icon-pencil"></i>
-                            <i class="icon-wrench"></i>
-                            <i class="icon-search"></i>
+                            <g:link class="icons-edit" action="edit" params='[id: "${data.medication_id }"]' title="Edit">
+                                <i class="icon-cog"></i>
+                                <i class="icon-pencil"></i>
+                                <i class="icon-wrench"></i>
+                            </g:link>
+                            <g:link class="icons-edit" action="delete" params='[id: "${data.medication_id }"]' onclick="return confirm('${message(code: 'default.button.delete.confirm.message', default: 'Are you sure?')}');" title="Delete">
+                                <i class="icon-remove"></i>
+                            </g:link>
                         </td>
-					</tr>
-					</g:each>
-				</tbody>
+                    </tr>
+                    </g:each>
+                </tbody>
             </table>
             
             
@@ -79,7 +82,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <g:each in="${result}" var="data">
+                    <g:each in="${UserMedicationsInstanceList }" var="data">
                     <tr>
                         <td>${data.name }</td>
                         <td>${data.dose }</td>
@@ -91,15 +94,20 @@
                         <td>${data.refill_date }</td>
                         <td>${data.comments }</td>
                         <td class="table-editdelete">
-                            <i class="icon-cog"></i>
-                            <i class="icon-pencil"></i>
-                            <i class="icon-wrench"></i>
-                            <i class="icon-search"></i>
+                            <g:link class="icons-edit" action="edit" params='[id: "${data.medication_id }"]' title="Edit">
+                                <i class="icon-cog"></i>
+                                <i class="icon-pencil"></i>
+                                <i class="icon-wrench"></i>
+                            </g:link>
+                            <g:link class="icons-edit" action="delete" params='[id: "${data.medication_id }"]' onclick="return confirm('${message(code: 'default.button.delete.confirm.message', default: 'Are you sure?')}');" title="Delete">
+                                <i class="icon-remove"></i>
+                            </g:link>
                         </td>
                     </tr>
                     </g:each>
                 </tbody>
             </table>
+            
             
         </div>
     </div>
