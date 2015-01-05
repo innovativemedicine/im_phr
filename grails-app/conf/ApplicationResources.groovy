98372ffeb44ef7814f9b13e4ebd551076e7b984c
imnderoo <nderoo.wong@gmail.com>
2015-01-05 17:47:32 -0500

    Added application.js to app's Resource bundle.

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,14 +1,9 @@
-modules =
-{
-    
-    
-    app
-    {
-      
-        dependsOn 'laf'
-        resource url: [  dir: 'css', file: 'main.css' ], attrs:[ rel: "stylesheet", type:'css' ], bundle: 'bundle_app'
-	
-    }
-    
-   
+modules = {
+
+	app {
+
+		dependsOn 'laf'
+		resource url: [  dir: 'js', file: 'application.js' ], bundle: 'bundle_app'
+		resource url: [  dir: 'css', file: 'main.css' ], attrs:[ rel: "stylesheet", type:'css' ], bundle: 'bundle_app'
+	}
 }
