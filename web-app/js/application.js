5e03392030012c3230c850416ace5785825b550e
imnderoo <nderoo.wong@gmail.com>
2015-01-05 17:46:09 -0500

    Moved JS from wellness to here

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -7,3 +7,45 @@ if (typeof jQuery !== 'undefined') {
 		});
 	})(jQuery);
 }
+
+$(function() {
+
+	$(".wellness-button").click(
+			function() {
+				$(this).siblings('.block-text').children('.form-hidden')
+						.children('.add-section').slideToggle();
+			});
+
+	$('#bmi-tooltip').tooltip();
+
+});
+
+// <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+//
+// // Load the Visualization API and the piechart package.
+// google.load('visualization', '1.0', {'packages':['corechart']});
+//	
+// // Set a callback to run when the Google Visualization API is loaded.
+// google.setOnLoadCallback(drawChart);
+//	
+//	
+// function drawChart() {
+//
+// var userCalories = '${UserCalories}';
+//	    
+// var data = google.visualization.arrayToDataTable([
+// ['Year', 'Sales', 'Expenses'],
+// ['2004', 1000, 400],
+// ['2005', 1170, 460],
+// ['2006', 660, 1120],
+// ['2007', 1030, 540]
+// ]);
+//	    
+// var options = {
+// title: 'Company Performance'
+// };
+//	    
+// var chart = new
+// google.visualization.LineChart(document.getElementById('chart_div'));
+// chart.draw(data, options);
+//	}
